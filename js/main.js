1a5c7ff2af6dd130c243fcacb42b9f210bebaf38
Yurii Yakubus <yakubusyurii@gmail.com>
2020-04-18 13:14:36 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+        responsive: [{
+            breakpoint: 441,
+            settings: {
+                arrows: false
+            }
+        }]
+    });
+});
+
+
